2ca260a502e61bc5bd4917319c332d147cf12fbf
YourGitHubUsername <mohammedmishalchungath@gmail.com>
2026-03-04 16:57:26 +0530

    Now the User could type a word and check if its a palindrome

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,18 +1,20 @@
+import java.util.Scanner;
+
 public class PalindroneCheckerApp {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
         System.out.println("Welcome to Palindrome Checker App");
 
-
-        String text = "madam";
-
+        System.out.println("Enter a String: ");
+        String text = sc.nextLine();
 
         String reversed = new StringBuilder(text).reverse().toString();
 
-        // Check if palindrome
-        if (text.equalsIgnoreCase(reversed)) {
-            System.out.println("The string \"" + text + "\" is a palindrome.");
-        } else {
-            System.out.println("The string \"" + text + "\" is NOT a palindrome.");
+        if(text.equalsIgnoreCase(reversed)){
+            System.out.println("The string is " + text + " a palindrome.");
+        }
+        else{
+            System.out.println("The string is " + text + " NOT a plaindrome.");
         }
     }
 }
