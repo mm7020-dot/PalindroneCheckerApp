48091bfabf448c2d46e7dea51c61980b892ebd41
YourGitHubUsername <mohammedmishalchungath@gmail.com>
2026-03-04 16:41:18 +0530

    UC-2: Here Harcoded String is used

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,6 +1,18 @@
 public class PalindroneCheckerApp {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version: 1.0");
+
+
+        String text = "madam";
+
+
+        String reversed = new StringBuilder(text).reverse().toString();
+
+        // Check if palindrome
+        if (text.equalsIgnoreCase(reversed)) {
+            System.out.println("The string \"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + text + "\" is NOT a palindrome.");
+        }
     }
 }
